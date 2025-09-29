42c02a889359857c1da9e219b580561843b8d7fa
Thomas <thomas@newtomsoft.fr>
2025-09-29 21:35:55 +0200

    add python files

---
+++ PythonSolver/Domain/Board/Island.py
@@ -0,0 +1,168 @@
+import typing
+
+from Domain.Board.Direction import Direction
+from Domain.Board.Position import Position
+
+IslandString0Bridge = typing.Literal[
+    '   ', ' X ', ' · '
+]
+IslandString1Bridge = typing.Literal[
+    ' ╶─', ' ╵ ', '─╴ ', ' ╷ ',
+]
+IslandString2Bridges = typing.Literal[
+    ' └─', '─┘ ', '─┐ ', ' ┌─',
+    ' │ ', '───',
+]
+IslandString3Bridges = typing.Literal[
+    '─┬─', ' ├─', '─┴─', '─┤ ',
+]
+IslandString4Bridges = typing.Literal[
+    '─┼─'
+]
+IslandString = typing.Union[
+    IslandString0Bridge,
+    IslandString1Bridge,
+    IslandString2Bridges,
+    IslandString3Bridges,
+    IslandString4Bridges
+]
+IslandWithBridgesString = typing.Union[
+    IslandString1Bridge,
+    IslandString2Bridges,
+    IslandString3Bridges,
+    IslandString4Bridges,
+]
+
+
+class Island:
+    def __init__(self, position: Position, bridges: int, positions_bridges: dict[Position, int] = None):
+        self.position = position
+        self.bridges_count = bridges
+        if bridges < 0 or bridges > 8:
+            raise ValueError("Bridges must be between 0 and 8")
+        self.direction_position_bridges: dict[Direction, tuple[Position, int]] = {}
+        if positions_bridges is not None:
+            for position, bridges in positions_bridges.items():
+                self.set_bridge_to_position(position, bridges)
+
+    @staticmethod
+    def from_str(position: Position, island_string: IslandString):
+        initial_position_bridges = {
+            position.up: 0,
+            position.down: 0,
+            position.left:  0,
+            position.right: 0,
+        }
+        island = Island(position, 0, initial_position_bridges)
+
+        match island_string:
+            case ' ╵ ':
+                island.set_bridge_to_direction(Direction.up(), 1)
+            case ' ╷ ':
+                island.set_bridge_to_direction(Direction.down(), 1)
+            case ' ╶─':
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '─╴ ':
+                island.set_bridge_to_direction(Direction.left(), 1)
+
+            case ' └─':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '─┘ ':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+            case '─┐ ':
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+            case ' ┌─':
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case ' │ ':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.down(), 1)
+            case '───':
+                island.set_bridge_to_direction(Direction.left(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+
+            case '─┬─':
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case ' ├─':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '─┴─':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+            case '─┤ ':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+
+            case '─┼─':
+                island.set_bridge_to_direction(Direction.up(), 1)
+                island.set_bridge_to_direction(Direction.down(), 1)
+                island.set_bridge_to_direction(Direction.left(), 1)
+                island.set_bridge_to_direction(Direction.right(), 1)
+
+        island.set_bridges_count_according_to_directions_bridges()
+        return island
+
+    def set_bridge_to_position(self, position: Position, number: int):
+        direction = self.position.direction_to(position)
+        self.direction_position_bridges[direction] = position, number
+
+    def set_bridge_to_direction(self, direction: Direction, number: int):
+        to_position = self.position.after(direction)
+        self.direction_position_bridges[direction] = to_position, number
+
+    def set_bridges_count_according_to_directions_bridges(self):
+        self.bridges_count = sum([bridges for (_, bridges) in self.direction_position_bridges.values()])
+
+    def has_no_bridge(self):
+        return self.bridges_count == 0
+
+    def __eq__(self, other):
+        return isinstance(other,
+                          Island) and self.position == other.position and self.bridges_count == other.bridges_count and self.direction_position_bridges == other.direction_position_bridges
+
+    def __repr__(self):
+        if self.has_no_bridge():
+            return ' · '
+        if self.bridges_number(Direction.up()) != 0 and self.bridges_number(Direction.down()) != 0 and self.bridges_number(Direction.left()) != 0 and self.bridges_number(
+                Direction.right()) != 0:
+            return '─┼─'
+        if self.bridges_number(Direction.up()) != 0 and self.bridges_number(Direction.left()) != 0 and self.bridges_number(Direction.right()) != 0:
+            return '─┴─'
+        if self.bridges_number(Direction.down()) != 0 and self.bridges_number(Direction.left()) != 0 and self.bridges_number(Direction.right()) != 0:
+            return '─┬─'
+        if self.bridges_number(Direction.up()) != 0 and self.bridges_number(Direction.down()) != 0 and self.bridges_number(Direction.left()) != 0:
+            return '─┤ '
+        if self.bridges_number(Direction.up()) != 0 and self.bridges_number(Direction.down()) != 0 and self.bridges_number(Direction.right()) != 0:
+            return ' ├─'
+        if self.bridges_number(Direction.up()) != 0 and self.bridges_number(Direction.left()) != 0:
+            return '─┘ '
+        if self.bridges_number(Direction.up()) != 0 and self.bridges_number(Direction.right()) != 0:
+            return ' └─'
+        if self.bridges_number(Direction.down()) != 0 and self.bridges_number(Direction.left()) != 0:
+            return '─┐ '
+        if self.bridges_number(Direction.right()) != 0 and self.bridges_number(Direction.down()) != 0:
+            return ' ┌─'
+        if self.bridges_number(Direction.up()) != 0 and self.bridges_number(Direction.down()) != 0:
+            return ' │ '
+        if self.bridges_number(Direction.down()) != 0:
+            return ' ╷ '
+        if self.bridges_number(Direction.up()) != 0:
+            return ' ╵ '
+        if self.bridges_number(Direction.right()) != 0 and self.bridges_number(Direction.left()) != 0:
+            return '───'
+        if self.bridges_number(Direction.right()) != 0:
+            return ' ╶─'
+        if self.bridges_number(Direction.left()) != 0:
+            return '─╴ '
+        return ' X '
+
+    def bridges_number(self, direction: Direction):
+        return self.direction_position_bridges.get(direction, (0, 0))[1]
---
+++ PythonSolver/Utils/ShapeGenerator.py
@@ -0,0 +1,72 @@
+﻿import itertools
+from typing import Collection
+
+from Domain.Board.Position import Position
+
+
+class ShapeGenerator:
+    @staticmethod
+    def get_all_shapes(rows_number, columns_number):
+        all_shapes = []
+        min_cells_number = rows_number + columns_number - 1
+        max_cells_number = rows_number * columns_number - int(rows_number / 2) * int(columns_number / 2)
+        for cells_number in range(min_cells_number, max_cells_number + 1):
+            generator = ShapeGenerator()
+            shapes = generator._generate_shapes(rows_number, columns_number, cells_number)
+            all_shapes.extend(shapes)
+        return all_shapes
+
+    @staticmethod
+    def around_shape(shape: Collection[Position]) -> set[Position]:
+        shape_set = set(shape)
+        enlarged_shape: set[Position] = set()
+        for position in shape:
+            for position_delta in [Position(-1, 0), Position(1, 0), Position(0, -1), Position(0, 1)]:
+                adjacent_cell = position + position_delta
+                if adjacent_cell not in shape_set:
+                    enlarged_shape.add(adjacent_cell)
+        return enlarged_shape
+
+    @staticmethod
+    def edges(shape: Collection[Position]) -> set[Position]:
+        shape_set = set(shape)
+        edge_positions: set[Position] = set()
+        for position in shape:
+            for neighbor_position in position.neighbors():
+                if neighbor_position not in shape_set:
+                    edge_positions.add(position)
+        return edge_positions
+
+    @staticmethod
+    def _generate_shapes(rows_number, columns_number, cells_number):
+        all_cells = [(r, c) for r in range(rows_number) for c in range(columns_number)]
+        return [set(combination) for combination in itertools.combinations(all_cells, cells_number)
+                if ShapeGenerator._covers_entire_grid(combination, rows_number, columns_number)
+                and ShapeGenerator._is_connected(combination)
+                and not ShapeGenerator._contains_square(combination)]
+
+    @staticmethod
+    def _covers_entire_grid(shape, rows_number, columns_number):
+        rows = {cell[0] for cell in shape}
+        cols = {cell[1] for cell in shape}
+        return len(rows) == rows_number and len(cols) == columns_number
+
+    @staticmethod
+    def _is_connected(shape):
+        visited = set()
+        ShapeGenerator._dfs(visited, shape)
+        return len(visited) == len(shape)
+
+    @staticmethod
+    def _dfs(visited, shape):
+        stack = [shape[0]]
+        while stack:
+            x, y = stack.pop()
+            if (x, y) not in visited:
+                visited.add((x, y))
+                stack.extend((nx, ny) for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)] if (nx := x + dx, ny := y + dy) in shape and (nx, ny) not in visited)
+
+    @staticmethod
+    def _contains_square(shape):
+        shape_set = set(shape)
+        return any((x + 1, y) in shape_set and (x, y + 1) in shape_set and (x + 1, y + 1) in shape_set for x, y in shape)
